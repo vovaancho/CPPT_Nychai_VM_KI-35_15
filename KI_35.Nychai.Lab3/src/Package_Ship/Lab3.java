326a1fa9ec9749301021cf36ea2a543aef1e0639
Volodymyr <volodymyr.nychai.ki.2020@lpnu.ua>
2022-09-28 20:41:21 +0300

    Create new project

---
+++ KI_35.Nychai.Lab3/src/Package_Ship/Lab3.java
@@ -0,0 +1,31 @@
+package Package_Ship;
+
+import java.io.FileNotFoundException;
+
+public class Lab3 {
+    public static void main(String []args) throws FileNotFoundException {
+
+
+        Ship ship2 = new Ship(520,10,7,6,2);
+        ship2.shipStartMoving();
+        ship2.setShipCount(30);
+        ship2.increaseCountOfPeople(20);
+        ship2.shipStartMoving();
+        ship2.reduseCountOfPeople(3);
+        ship2.shipStartMoving();
+        ship2.getCountOfPeople();
+
+
+        ship2.getInfoAboutLocationOfShip();
+        ship2.setLocationOfShip(78,98);
+        ship2.getInfoAboutLocationOfShip();
+        ship2.reduseAllLocation(20,20);
+        ship2.increaseAllLocation(20,20);
+
+        ship2.showEngineOfShip();
+        ship2.increasePowerOfShip(50);
+        ship2.redusePowerOfShip(70);
+
+        ship2.AllInfo();
+    }
+}
